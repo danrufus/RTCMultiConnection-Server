a9328e43d70acdd528855f69998f98ff1e225754
libin <libin@belink.com>
2019-07-15 16:29:28 +0800

    modify

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-// http://127.0.0.1:9001
-// http://localhost:9001
+// http://127.0.0.1:9009
+// http://localhost:9009
 
 const fs = require('fs');
 const path = require('path');
@@ -9,7 +9,7 @@ var httpServer = require('http');
 const ioServer = require('socket.io');
 const RTCMultiConnectionServer = require('./node_scripts/index.js');
 
-var PORT = 9001;
+var PORT = 9009;
 var isUseHTTPs = false;
 
 const jsonPath = {
@@ -26,7 +26,7 @@ config = getBashParameters(config, BASH_COLORS_HELPER);
 
 // if user didn't modifed "PORT" object
 // then read value from "config.json"
-if(PORT === 9001) {
+if(PORT === 9009) {
     PORT = config.port;
 }
 if(isUseHTTPs === false) {
